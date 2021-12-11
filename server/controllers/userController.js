47754c9dce7b9d723f5342af5c53b627db85913e
ernestocruz <ecruz64@me.com>
2021-12-10 19:45:53 -0500

    adding the finishing touches

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,7 +16,7 @@ exports.view = (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err; // not connected
     console.log('Connected as ID ' + connection.threadId);
-    // User the connection
+    // Equip connection
     connection.query('SELECT * FROM gear WHERE status = "active"', (err, rows) => {
       // when done with the connection, release it
       connection.release();
@@ -40,7 +40,7 @@ exports.find = (req, res) => {
     if (err) throw err; // not connected
     console.log('Connected as ID ' + connection.threadId);
     let searchTerm = req.body.search;
-    // User the connection
+    // Equipment  connection
     connection.query('SELECT * FROM gear WHERE equip_name LIKE ? OR mfr LIKE ?', ['%' + searchTerm + '%', '%' + searchTerm + '%'], (err, rows) => {
       // when done with the connection, release it
       connection.release();
@@ -57,7 +57,7 @@ exports.form = (req,res) => {
   res.render('add-equip');
 }
 
-// Add New User
+// Add New Equipment
 exports.create = (req, res) => {
   const {equip_name, mfr, serial_num, vendor, descr} = req.body;
 
